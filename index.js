7a35f2b3567780aee12ad78f1a001a2b08ccdd00
Mayank <kaushalmayank.k@gmail.com>
2022-12-13 19:09:55 +0530

    Final Commit
    Minor fixes and arrow rotation js added.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function toggleFilters(){
-  const item = document.getElementById("filters");
-  if (item.classList.contains("noDisplay")){
-    item.classList.remove("noDisplay");
+  const filters = document.getElementById
+  ("filters");
+  const arrow = document.getElementById
+  ("filtersButtonArrow");
+  
+  if (filters.classList.contains("noDisplay")){
+    filters.classList.remove("noDisplay");
+    arrow.classList.add('arrowUp');
+    arrow.classList.remove('arrowDown');
   }
   else{
-    item.classList.add("noDisplay");
+    filters.classList.add("noDisplay");
+    arrow.classList.remove('arrowUp');
+    arrow.classList.add('arrowDown');
   }
 }
